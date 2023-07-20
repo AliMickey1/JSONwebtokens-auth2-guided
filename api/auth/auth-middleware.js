84dd718dec8daa58551480ca20e5f4e85a52e904
Aliceson Phillips <alicesonphillips@gmail.com>
2023-07-20 12:24:22 -0400

    Completed GP

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,10 +1,29 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../../config')
 // AUTHENTICATION
 const restricted = (req, res, next) => {
-  next()
+  const token = req.headers.authorization
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if(err) {
+        next({ status: 401, message: `token bad: ${err.message}`})
+      } else {
+        req.decodedJWT = decoded
+        next()
+      }
+    })
+  } else {
+    next ({ status: 401, message: 'what? no token?'})
+  }
 }
 
 // AUTHORIZATION
-const checkRole = (req, res, next) => {
+const checkRole = role => (req, res, next) => {
+  if(req.decodedJWT && req.decodedJWT.role === role) {
+    next()
+  } else {
+    next({ status: 403, message: `you have no power here!`})
+  }
   next()
 }
 
