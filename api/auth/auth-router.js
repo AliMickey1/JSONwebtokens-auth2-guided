283c41dfabd43d3fe3661f590a6a5c99af916aad
Aliceson Phillips <alicesonphillips@gmail.com>
2023-07-20 20:26:52 -0400

    Complete after Module 2

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,33 +1,39 @@
+const router = require('express').Router()
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const router = require('express').Router()
-const User = require('../users/users-model.js')
+const Users = require('../users/users-model.js')
+const secrets = require('../../config/index.js')
 
-const { BCRYPT_ROUNDS, JWT_SECRET } = require('../../config')
-
+// for endpoints beginning with /api/auth
 router.post('/register', (req, res, next) => {
   let user = req.body
-
-  // bcrypting the password before saving
-  const hash = bcrypt.hashSync(user.password, BCRYPT_ROUNDS)
-  // never save the plain text password in the db
+  const hash = bcrypt.hashSync(user.password, 8) // 2 ^ n
   user.password = hash
 
-  User.add(user)
+  Users.add(user)
     .then(saved => {
-      res.status(201).json({ message: `Great to have you, ${saved.username}` })
+      res.status(201).json({
+        message: `Great to have you with us, ${saved.username}`
+      })
     })
-    .catch(next) // our custom err handling middleware in server.js will trap this
+    .catch(next) // our custom err handling middleware will trap this
 })
 
 router.post('/login', (req, res, next) => {
   let { username, password } = req.body
 
-  User.findBy({ username })
-    .then(([user]) => {
+  Users.findBy({ username })
+    .first()
+    .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = buildToken(user)
-        res.status(200).json({ message: `Welcome back ${user.username}...`, token })
+        // this is the critical line. Session saved, cookie set on client:
+        // req.session.user = user
+                
+        const token = generateToken(user)
+        res.status(200).json({
+          message: `Welcome back ${user.username}, have a cookie!`,
+          token,
+        })
       } else {
         next({ status: 401, message: 'Invalid Credentials' })
       }
@@ -35,16 +41,35 @@ router.post('/login', (req, res, next) => {
     .catch(next)
 })
 
-function buildToken(user) {
+router.get('/logout', (req, res) => {
+  if (req.session.user) {
+    const { username } = req.session.user
+    req.session.destroy(err => {
+      if (err) {
+        res.json({ message: `You can never leave, ${username}...` })
+      } else {
+        // the following line is optional: compliant browsers will delete the cookie from their storage
+        res.set('Set-Cookie', 'monkey=; SameSite=Strict; Path=/; Expires=Thu, 01 Jan 1970 00:00:00')
+        res.json({ message: `Bye ${username}, thanks for playing` })
+      }
+    })
+  } else {
+    res.json({ message: 'Excuse me, do I know you?' })
+  }
+})
+
+function generateToken(user) {
   const payload = {
     subject: user.id,
     username: user.username,
-    role: user.role,
   }
+  // const secret = 'askdkgndklsslitndlkdkfjd'
+
   const options = {
-    expiresIn: '1d', //1 day
+    expiresIn: '8h',
   }
-  return jwt.sign(payload, JWT_SECRET , options) //second parameter is a secret string that no one can know (look in index in config folder)
+
+  return jwt.sign(payload, secrets.JWT_SECRET, options)
 }
 
 module.exports = router
